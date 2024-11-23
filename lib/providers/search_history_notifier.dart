b02453abea2d38fb2befbd2af9c35b5839fbddf6
OuOu2021 <1113117424@qq.com>
2024-11-23 13:12:58 +0800

    feat: save search history

--- lib/providers/search_history_notifier.dart
+++ lib/providers/search_history_notifier.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SearchHistoryNotifier extends ChangeNotifier {
-  final List<String> _history = [];
+  List<String> _history = ['[help]'];
+  static const String _historyCountKey = 'searchHistory';
   // List.generate(10, (index) => 'test${index + 1}');
 
   List<String> get history => List.unmodifiable(_history);
@@ -11,31 +13,53 @@ class SearchHistoryNotifier extends ChangeNotifier {
   String get last => _history.last;
   int get length => _history.length;
 
-  void add(String item) {
+  SearchHistoryNotifier() {
+    _loadHistory().then((_) => notifyListeners());
+  }
+
+  Future<void> _loadHistory() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey(_historyCountKey)) {
+      List<String> hisotry = prefs.getStringList(_historyCountKey)!;
+      _history = hisotry;
+    }
+  }
+
+  Future<void> _saveHistory() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setStringList(_historyCountKey, _history);
+  }
+
+  Future<void> add(String item) async {
     _history.add(item);
+    await _saveHistory();
     notifyListeners();
   }
 
-  void addToHead(String item) {
+  Future<void> addToHead(String item) async {
     _history.insert(0, item);
     final logger = Logger();
     logger.d(history);
+    await _saveHistory();
     notifyListeners();
   }
 
-  void clear() {
+  Future<void> clear() async {
     _history.clear();
+    await _saveHistory();
     notifyListeners();
   }
 
-  void remove(String item) {
+  Future<void> remove(String item) async {
     _history.remove(item);
+    await _saveHistory();
     notifyListeners();
   }
 
-  void removeLast() {
+  Future<void> removeLast() async {
     if (_history.isNotEmpty) {
       _history.removeLast();
+      await _saveHistory();
       notifyListeners();
     }
   }
